5b999ab4eaba3b51bcf9ba04124242a33cf9a21d
Simon Tech <contato.simon.tech@gmail.com>
2026-01-09 18:54:32 -0300

    feat: update server port to 8080 and add EJS view for questions forum

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 8080;
+
+// Configuração do EJS
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render("index");
 });
 
 app.listen(port, () => {
