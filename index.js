8ae10ed57cfd0852f69e503f938517398b1f2c16
Simon Tech <contato.simon.tech@gmail.com>
2026-01-09 20:19:53 -0300

    feat: simplify index route and update index view with Bootstrap styling

--- index.js
+++ index.js
@@ -6,28 +6,8 @@ const port = 8080;
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/:nome/:idade/:empresa', (req, res) => {
-    const nome = req.params.nome;
-    const idade = req.params.idade;
-    const empresa = req.params.empresa;
-
-    const exibirMsg = false;
-
-    const produtos = [
-        { nome: "Notebook", preco: 2500 },
-        { nome: "Smartphone", preco: 1500 },
-        { nome: "Tablet", preco: 800 },
-        { nome: "Monitor", preco: 1200 },
-        { nome: "Teclado", preco: 200 }
-    ];
-
-    res.render("index", {
-        nome: nome,
-        idade: idade,
-        empresa: empresa,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get('/', (req, res) => {
+    res.render("index");
 });
 
 app.listen(port, () => {
