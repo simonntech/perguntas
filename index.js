e312b57cf32f3e22a1bd1ec7fb6612be04e9fd55
Simon Tech <contato.simon.tech@gmail.com>
2026-01-09 20:31:57 -0300

    feat: add perguntar route and create corresponding EJS view for question submission

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.render("index");
 });
 
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar");
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
