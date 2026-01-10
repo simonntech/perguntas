a29a9b77d16e13854f1c324b51b821e88697ff98
Simon Tech <contato.simon.tech@gmail.com>
2026-01-10 17:10:03 -0300

    feat: update perguntar view to include input fields for title and description; modify index.js for improved formatting and add MySQL and Sequelize dependencies

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = 8080;
 
 // Configuração do EJS
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
 // Configuração do Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Rotas
-app.get('/', (req, res) => {
-    res.render("index");
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
-    res.render("perguntar");
+  res.render("perguntar");
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    // Lógica para salvar a pergunta no banco de dados
-    res.send("Pergunta salva com sucesso!");
+  // Lógica para salvar a pergunta no banco de dados
+  const titulo = req.body.titulo;
+  const descricao = req.body.descricao;
+  res.send("Pergunta salva com sucesso!");
 });
 
 app.listen(port, () => {
