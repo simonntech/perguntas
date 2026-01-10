006862e897d38d797968f535465ebd6c15888c55
Simon Tech <contato.simon.tech@gmail.com>
2026-01-10 16:53:34 -0300

    feat: add body-parser middleware and implement question saving route; update perguntar form to POST data

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
 
 // Configuração do EJS
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+// Configuração do Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+// Rotas
 app.get('/', (req, res) => {
     res.render("index");
 });
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    // Lógica para salvar a pergunta no banco de dados
+    res.send("Pergunta salva com sucesso!");
 });
 
 app.listen(port, () => {
